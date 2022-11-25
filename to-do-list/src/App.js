cbd69ccda4d5487dbd6c8ffc2de173b2aa40a8f6
coolblast-coder <lyapunovantik@gmail.com>
2022-11-25 10:16:47 -0700

    Added header, menu and list of 3 todolist items

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,11 +1,67 @@
-import logo from './logo.svg';
-import './style/App.css';
-import 'bootstrap/dist/css/bootstrap.css'
+import logo from "./logo.svg";
+import "./style/App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-     <h1 className='text-center'>Hello world!</h1>
+      <div className="container">
+        <div className="row">
+          <div className="todolist">
+            <h1>Things to do</h1>
+            <ul className="list-unstyled">
+              <li className="list-group-item">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="todo-item-check-1"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="todo-item-check-1"
+                  >
+                    Take out the trash
+                  </label>
+                </div>
+              </li>
+              <li className="list-group-item">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="todo-item-check-2"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="todo-item-check-2"
+                  >
+                    Buy bread
+                  </label>
+                </div>
+              </li>
+              <li className="list-group-item">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="todo-item-check-3"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="todo-item-check-3"
+                  >
+                    Teach penguins to fly
+                  </label>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
