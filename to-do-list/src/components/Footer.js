5a1105943a23241bc8796337ea55a8c4637936a4
coolblast-coder <lyapunovantik@gmail.com>
2022-11-25 13:17:08 -0700

    Added counter to Footer

--- to-do-list/src/components/Footer.js
+++ to-do-list/src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function Footer(props){
+    const {count} = props
     return(
+        
         <div className="todo-footer">
-            <strong>
-                <span className="count-todos">0</span>
-            </strong>
+                <span className="count-todos">{count}</span>
             {' items left'}
         </div>
     )
--- to-do-list/src/components/TodoList.js
+++ to-do-list/src/components/TodoList.js
@@ -1,21 +1,21 @@
 import React from "react";
-import Header from './Header'
+import Header from "./Header";
 import TodoItem from "./TodoItem";
 import Footer from "./Footer";
 
 function TodoList(props) {
   const { title, items } = props;
-
+  const count = items.length;
   return (
     <div className="todolist">
-     {/* <h1>{title.toUpperCase()}</h1> */}
-<Header title = {title}/>
+      {/* <h1>{title.toUpperCase()}</h1> */}
+      <Header title={title} />
       <ul className="list-group list-group-flush">
         {items.map((item) => (
-          <TodoItem item = {item}/>
+          <TodoItem item={item} />
         ))}
       </ul>
-      <Footer/>
+      <Footer count={count}/>
     </div>
   );
 }
